4c9df32f8a648d6141120210a33f005a4ddc80b4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-22 00:00:19 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import './app.css';
 
 function App() {
   return (
     <div >
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+      </div>
   
     </div>
   );
